6b675beda9ce8d42968b956ac3b0bd7a2c659fb1
Anul Kumar <anulkumar@gmail.com>
2025-03-19 21:02:39 +0530

    TDD addition

--- string_calculator.rb
+++ string_calculator.rb
@@ -36,3 +36,32 @@ puts 'input: "1,5", output: '+add("1,5").to_s
 puts 'input: "1\n2,3", output: '+add("1\n2,3").to_s
 puts 'input: "//;\n1;2", output: '+add("//;\n1;2").to_s
 puts add("1,-5")
+
+
+### TDD
+
+require 'minitest/autorun'
+
+describe "string calculator"  do
+  it "returns 0 when input is empty string"  do
+	  assert_equal(add(""), 0)
+  end
+  it "returns 1 when input is '1'"  do
+	  assert_equal(add("1"), 1)
+  end
+  it "returns 6 when input is '1,5'"  do
+	  assert_equal(add("1,5"), 6)
+  end
+  it "returns 6 when input is '1\n2,3'"  do
+	  assert_equal(add("1\n2,3"), 6)
+  end
+  it "returns 3 when input is '//;\n1;2'"  do
+	  assert_equal(add("//;\n1;2"), 3)
+  end
+  it "returns 3 when input is negative numbers"  do
+	  assert_equal(add("//;\n1;2"), 3)
+  end
+  it "returns nil when input contains negative number"  do
+	  assert_equal(add("1,-5"), nil)
+  end
+end
